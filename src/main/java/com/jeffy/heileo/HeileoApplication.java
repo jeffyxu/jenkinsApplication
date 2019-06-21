05109b936e0094f1d6fa58982bae0ebafa62ae34
kaixu <kaixumvp.163.com>
2019-06-21 21:15:20 +0800

    添加工程

---
+++ src/main/java/com/jeffy/heileo/HeileoApplication.java
@@ -0,0 +1,13 @@
+package com.jeffy.heileo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeileoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HeileoApplication.class, args);
+    }
+
+}
